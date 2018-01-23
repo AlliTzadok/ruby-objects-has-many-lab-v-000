d25cf6c59edd5442a59ace07d556087c9685c4e9
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 16:19:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,9 +16,13 @@ class Artist
 
   end
 
-  def add_song_by_name(song)
+  def add_song_by_name(name)
     song = Song.new(name)
-    
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
 
 
 
